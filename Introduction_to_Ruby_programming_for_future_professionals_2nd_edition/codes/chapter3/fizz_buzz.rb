8044bba9658abc044dc75803992f2f51e2d2cf98
naga-naga <kiyoharu.639721@gmail.com>
2024-06-25 08:58:04 +0900

    『プロを目指す人のためのRuby入門』第3章

---
+++ Introduction_to_Ruby_programming_for_future_professionals_2nd_edition/codes/chapter3/fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(n)
+  if n % 15 == 0
+    "Fizz Buzz"
+  elsif n % 5 == 0
+    "Buzz"
+  elsif n % 3 == 0
+    "Fizz"
+  else
+    n.to_s
+  end
+end
+
+# 30.times do |n|
+#   puts fizz_buzz(n)
+# end
