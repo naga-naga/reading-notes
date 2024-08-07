cca6363b4c55dd72f5ff0600a839e9cce4cf54c2
naga-naga <kiyoharu.639721@gmail.com>
2024-08-08 00:46:52 +0900

    『プロを目指す人のためのRuby入門』10.1節～10.2節

---
+++ Introduction_to_Ruby_programming_for_future_professionals_2nd_edition/codes/chapter10/memo.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+def greet
+  puts 'hello'
+
+  if block_given?
+    text = yield 'arg'
+    puts text
+  end
+
+  puts 'bye'
+end
+
+# greet
+# puts
+
+greet do |text|
+  puts "foo! #{text}"
+  puts text * 3
+  'return value'
+end
+
+puts '---'
+
+# 明示的なブロック受け取り
+def greet2(&block)
+  block&.call
+end
+
+greet2 do
+  puts 'hello!'
+end
+
+puts '---'
+
+def greet_ja(&block)
+  texts = ['おはよう', 'こんにちは', 'こんばんは'] # rubocop:disable Style/WordArray
+  greet_common(texts, &block)
+end
+
+def greet_en(&block)
+  texts = ['good morning', 'hello', 'good evening']
+  greet_common(texts, &block)
+end
+
+def greet_common(texts, &block)
+  puts texts[0]
+  puts block.call(texts[1])
+  puts texts[2]
+end
+
+greet_ja do |text|
+  text * 2
+end
+
+greet_en(&:upcase)
+
+puts '---'
+
+# arity
+def greet_arity(&block) # rubocop:disable Metrics/MethodLength
+  puts 'おはよう'
+  text =
+    if block.arity == 1
+      yield 'こんにちは'
+    elsif block.arity == 2
+      yield 'こんに', 'ちは'
+    else
+      yield
+    end
+  puts text
+  puts 'こんばんは'
+end
+
+greet_arity do |text|
+  text * 2
+end
+
+greet_arity do |text1, text2|
+  text1 * 2 + text2 * 2
+end
