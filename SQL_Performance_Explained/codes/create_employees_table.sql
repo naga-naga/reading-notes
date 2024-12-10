06a523ba9c501f74e0bb3e4fa42bc1a1987b4a17
naga-naga <imanagak@bookwalker.co.jp>
2024-12-10 18:41:09 +0900

    『SQLパフォーマンス詳解』の employees テーブルを作成するクエリを追加

---
+++ SQL_Performance_Explained/codes/create_employees_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(1000) NOT NULL,
+  last_name VARCHAR(1000) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  phone_number VARCHAR(1000) NOT NULL
+);
