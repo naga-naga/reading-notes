59269778bf11ad2222134c14fddcbfb9b1a3eabe
naga-naga <kiyoharu.639721@gmail.com>
2025-07-10 18:40:01 +0900

    『プロを目指す人のための TypeScript 入門』3.1~3.2

--- Introduction_to_TypeScript_for_future_professionals/codes/src/index.ts
+++ Introduction_to_TypeScript_for_future_professionals/codes/src/index.ts
@@ -12,3 +12,59 @@ console.log(Number(true));
 console.log(Number('foo'));
 // console.log(BigInt('foo')); // 死ぬ
 console.log(+'123'); // + でも変換できる
+
+// オブジェクト
+const username = 'Alice';
+const propName = 'age';
+const user = {
+  username, // 省略記法
+  '🫠': 'melt', // 文字列プロパティ
+  [propName]: 20, // 動的プロパティ
+}
+console.log(user);
+
+const user2 = {
+  // username: 'Bob', // これは上書きされるのでエラーになる
+  ...user, // スプレッド構文
+  email: '',
+}
+console.log(user2);
+
+// オブジェクトの型
+const obj: {
+  username: string;
+  age: number;
+} = {
+  username: 'Alice',
+  age: 20,
+  // age: true, // エラー
+};
+
+// 型に名前を付ける
+type User = {
+  readonly id: number;
+  username: string;
+  age: number;
+  email?: string; // string | undefined
+};
+const user3: User = {
+  id: 1,
+  username: 'Charlie',
+  age: 25,
+}
+console.log(user3.email?.toUpperCase()); // undefined の場合があるので ?. でアクセス
+// user3.id = 2; // readonly なのでエラー
+
+// インデックスシグネチャ
+type PriceData = {
+  [key: string]: number; // 任意の文字列キーの型が number
+};
+const data: PriceData = {
+  apple: 100,
+  banana: 200,
+  // orange: '300', // エラー
+};
+
+// typeof
+const commands = ['start', 'stop', 'pause'] as const;
+type Command = typeof commands[number];
