46aff58b3db97387317611710a0d0e4b7dbb645b
naga-naga <kiyoharu.639721@gmail.com>
2025-07-11 18:31:52 +0900

    『プロを目指す人のための TypeScript 入門』3.3~3.4 型まわり

--- Introduction_to_TypeScript_for_future_professionals/codes/src/index.ts
+++ Introduction_to_TypeScript_for_future_professionals/codes/src/index.ts
@@ -68,3 +68,74 @@ const data: PriceData = {
 // typeof
 const commands = ['start', 'stop', 'pause'] as const;
 type Command = typeof commands[number];
+
+// subtype
+type FooBar = {
+  foo: string;
+  bar: number;
+};
+type FooBarBaz = {
+  foo: string;
+  bar: number;
+  baz: boolean;
+};
+const fooBarBazObj: FooBarBaz = {
+  foo: 'hello',
+  bar: 42,
+  baz: true,
+};
+const fooBarObj: FooBar = fooBarBazObj; // コンパイルエラーは出ない
+
+// 余剰プロパティ
+type User2 = {
+  name: string;
+  age: number;
+};
+const user2_1: User2 = {
+  name: 'Alice',
+  age: 30,
+  // email: '', // リテラルの代入時は余剰プロパティはエラー
+};
+const user2_obj = {
+  name: 'Alice',
+  age: 30,
+  email: ''
+};
+const user2_2: User2 = user2_obj; // 変数に入れて渡すと余剰プロパティチェックはされない
+
+// 型引数
+type List<T> = {
+  items: T[];
+};
+type Family<Parent, Child> = { // 2文字以上も可
+  mother: Parent;
+  father: Parent;
+  children: Child[];
+};
+
+// extends
+type HasName = {
+  name: string;
+};
+type Family2<Parent extends HasName, Child extends HasName> = {
+  mother: Parent;
+  father: Parent;
+  children: Child[];
+};
+// type T = Family2<number, string>; // number は HasName を満たさないのでエラー
+// type T = Family2<User2, User2>; // User2 は HasName を満たすのでOK
+type Animal2 = {
+  name: string;
+};
+type Human2 = {
+  name: string;
+  age: number;
+};
+// type T = Family2<Animal2, Human2>; // Animal2 は HasName を満たすのでOK
+
+// オプショナルな型引数
+type List2<T, U = string> = {
+  items: T[];
+  description: U; // オプショナルな型引数
+};
+// type T = List2<number>; // U は string になる
