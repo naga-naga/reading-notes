42801bc1c39e3278dd29495d3d5ad7fb63834ee8
naga-naga <kiyoharu.639721@gmail.com>
2025-11-21 00:46:29 +0900

    『詳解 Terraform』2.5 入力変数と出力変数

--- Terraform_Up_and_Running_3rd_edition/codes/chapter2/main.tf
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter2/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+variable "server_port" {
+  description = "HTTP リクエストを待ち受けるポート"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-0e68e34976bb4db93"
   instance_type          = "t3.micro"
@@ -11,7 +17,7 @@ resource "aws_instance" "example" {
               #!/bin/bash
               cd /home/ec2-user
               echo "Hello World" > index.html
-              nohup python3 -m http.server 8080 &
+              nohup python3 -m http.server ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -25,9 +31,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  description = "EC2 インスタンスの Public IP アドレス"
+  value = aws_instance.example.public_ip
 }
