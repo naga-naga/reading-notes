d2dbeed0d2c9160b5a028ca253dcb8d58c87870a
naga-naga <kiyoharu.639721@gmail.com>
2025-11-21 01:00:54 +0900

    『詳解 Terraform』2.6 Web サーバクラスタのデプロイ
    - output が古いままなのでこの状態では動かない
    - 起動設定はもう使えないらしい？ 起動テンプレートを使った書き方ができないか

--- Terraform_Up_and_Running_3rd_edition/codes/chapter2/main.tf
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter2/main.tf
@@ -8,10 +8,10 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0e68e34976bb4db93"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0e68e34976bb4db93"
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -20,10 +20,8 @@ resource "aws_instance" "example" {
               nohup python3 -m http.server ${var.server_port} &
               EOF
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -38,7 +36,32 @@ resource "aws_security_group" "instance" {
   }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
 output "public_ip" {
   description = "EC2 インスタンスの Public IP アドレス"
-  value = aws_instance.example.public_ip
+  value       = aws_instance.example.public_ip
 }
