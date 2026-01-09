73b7e3cd13ec82300bd70abd69b2739e71e9a850
naga-naga <kiyoharu.639721@gmail.com>
2026-01-09 17:38:44 +0900

    『詳解 Terraform』4.2 data-storesをmodule化してみる

---
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter4/modules/data-stores/mysql/main.tf
@@ -0,0 +1,21 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "naga-terraform-up-and-running-3rd-edition"
+#     key            = "stage/data-stores/mysql/terraform.tfstate"
+#     region         = "ap-northeast-1"
+#     dynamodb_table = "terraform-up-and-running-locks"
+#     encrypt        = true
+#   }
+# }
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
