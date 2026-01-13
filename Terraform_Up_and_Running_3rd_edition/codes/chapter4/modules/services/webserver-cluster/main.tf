0168f5e6a095298a2e77917b7568d48db868ca0f
naga-naga <kiyoharu.639721@gmail.com>
2026-01-13 17:35:46 +0900

    『詳解 Terraform』4.3 locals

--- Terraform_Up_and_Running_3rd_edition/codes/chapter4/modules/services/webserver-cluster/main.tf
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter4/modules/services/webserver-cluster/main.tf
@@ -1,3 +1,12 @@
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
+
+
 # s3バケットを作ると片付けるのが大変なのでlocalにしておく
 data "terraform_remote_state" "db" {
   backend = "local"
@@ -83,17 +92,17 @@ resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
@@ -106,7 +115,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
