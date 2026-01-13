e13ad7be68f1c65463e5dc17013744ce845b28ce
naga-naga <kiyoharu.639721@gmail.com>
2026-01-13 17:50:12 +0900

    『詳解 Terraform』4.4 モジュールの出力変数

--- Terraform_Up_and_Running_3rd_edition/codes/chapter4/modules/services/webserver-cluster/outputs.tf
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter4/modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,13 @@ output "public_ip" {
   description = "EC2 インスタンスの Public IP アドレス"
   value       = aws_lb.example.dns_name
 }
+
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "Auto Scaling Groupの名前"
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "ロードバランサのドメイン名"
+}
--- Terraform_Up_and_Running_3rd_edition/codes/chapter4/prod/services/webserver-cluster/main.tf
+++ Terraform_Up_and_Running_3rd_edition/codes/chapter4/prod/services/webserver-cluster/main.tf
@@ -5,11 +5,33 @@ provider "aws" {
 module "webserver-cluster" {
   source = "../../../modules/services/webserver-cluster"
 
-  cluster_name = "webservers-prod"
+  cluster_name           = "webservers-prod"
   db_remote_state_bucket = "naga-terraform-up-and-running-3rd-edition"
-  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
 
   instance_type = "t2.micro" # 本来ならもっと大きくする
-  min_size = 2
-  max_size = 10
+  min_size      = 2
+  max_size      = 10
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale_out_during_business_hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
+  time_zone             = "Asia/Tokyo"
+
+  autoscaling_group_name = module.webserver-cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale_in_at_night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+  time_zone             = "Asia/Tokyo"
+
+  autoscaling_group_name = module.webserver-cluster.asg_name
 }
