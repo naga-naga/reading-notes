a80c23961605f2c4833fc0e4038c2efa6fe7bb4e
naga-naga <kiyoharu.639721@gmail.com>
2026-04-09 17:55:51 +0900

    『AWSではじめるインフラ構築入門 第2版』4.3 サブネットを作成

--- Getting_Started_with_AWS_Infrastructure_2nd_edition/codes/main.tf
+++ Getting_Started_with_AWS_Infrastructure_2nd_edition/codes/main.tf
@@ -9,3 +9,47 @@ resource "aws_vpc" "main" {
     Name = "sample-vpc"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.0.0/20"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "sample-subnet-public01"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.16.0/20"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "sample-subnet-public02"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.64.0/20"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "sample-subnet-private01"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = "10.0.80.0/20"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "sample-subnet-private02"
+  }
+}
